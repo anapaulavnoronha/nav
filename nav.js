a7cc4232028b5b390c4948e533c551e8324ab77e
anapaulavnoronha <anapaulavnoronha@gmail.com>
2019-03-11 16:09:38 +0000

    Adding nav js to click the icon

---
+++ nav.js
@@ -0,0 +1,13 @@
+/*let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});*/
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
